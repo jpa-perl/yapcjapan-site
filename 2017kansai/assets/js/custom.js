397a0a15f0fe98956253cad95b51440a365d524e
karupanerura <karupa@cpan.org>
2017-01-17 01:26:40 +0900

    Revert "added talks list page (for preview)"
    This reverts commit c2e4bd294342b1b2ad388be7a9b2038054f55f0d.

--- 2017kansai/assets/js/custom.js
+++ 2017kansai/assets/js/custom.js
@@ -1,54 +1,24 @@
-(function (Vue, $) {
-  var JSON_URL = {
-    pending: 'https://spreadsheets.google.com/feeds/list/1e16uUbDJGclGdrJzVl-EFr8ns2J3S2WNaAEA151NsQk/od6/public/values?alt=json'
-  };
-
-  function fetchTalkProposals (type, cb) {
-    var url = JSON_URL[type];
-
-    var talks = [];
-    $.get(url, function (json) {
-      var entries = json.feed.entry;
-      for (var i = 0, l = entries.length; i < l; i ++) {
-        var entry = entries[i];
-        var talk = {
-          id:          entry['gsx$id']['$t'],
-          title:       entry['gsx$title']['$t'],
-          description: entry['gsx$description']['$t'],
-          time:        entry['gsx$talktime']['$t'],
-          author: {
-            name:      entry['gsx$author']['$t'],
-            githubId:  entry['gsx$githubid']['$t'],
-            twitterId: entry['gsx$twitterid']['$t'],
-            blogUrl:   entry['gsx$blogurl']['$t']
-          }
-        };
-
-        // sanitize
-        if (! talk.author.blogUrl.match(/^https?:/)) {
-          // It makes XSS maybe, so ignored.
-          talk.author.blogUrl = null;
-        }
-
-        // convert id to url
-        talk.author.githubUrl  = "https://github.com/"+encodeURIComponent(talk.author.githubId);
-        talk.author.twitterUrl = "https://twitter.com/"+encodeURIComponent(talk.author.twitterId);
-
-        talk.articleId = 'talk-'+talk.id;
-        talk.url       = '#'+encodeURIComponent(talk.articleId);
-
-        talks.push(talk);
-      }
-      if (cb) cb(talks);
-    });
-
-    return talks;
-  }
-
-  var PendingTalks = new Vue({
-    el: '#pending-talks',
-    data: {
-      talks: fetchTalkProposals('pending')
-    }
-  });
-})(Vue, $);
+// Blog Entries from atom feed
+//
+// (function (Vue, $) {
+//   var BlogEntries = new Vue({
+//     el: '#blog-entries',
+//     data: {
+//       loaded:  false,
+//       entries: []
+//     },
+//     created: function () {
+//       var v = this;
+//       $.get('http://blog.yapcjapan.org/feed', function (xml) {
+//         var entries = xml.getElementsByTagName("entry");
+//         for (var i = 0, l = entries.length; i < l; i++) {
+//           var title = (entries[i].getElementsByTagName("title"))[0].textContent;
+//           var url   = (entries[i].getElementsByTagName("link"))[0].getAttribute('href');
+//           var date  = (entries[i].getElementsByTagName("published"))[0].textContent.replace(/T.*/, '');
+//           v.entries.push({ title: title, url: url, date: date });
+//         }
+//         v.loaded = true;
+//       });
+//     }
+//   });
+// })(Vue, $);
