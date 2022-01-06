8bbc9151e52cb407216d45c8d8cced2c6fd895b3
nagayama <nagayama@iroirodesign.com>
2022-01-06 22:42:58 +0900

    prepare for coding

---
+++ 2022online/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./*.mustache'],
+  theme: {
+    colors: {
+      primary: '#5A3434',
+      tint: '#ED2628',
+      pink: '#F05A59',
+      bg: '#EFEFEF',
+      'bg-light': '#F6F6F6',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
