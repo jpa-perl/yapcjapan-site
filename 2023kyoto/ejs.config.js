753de312f8bffaa2f8a8168845107a3d44fd567d
nagayama <nagayama@iroirodesign.com>
2022-12-27 09:18:27 +0900

    add dummy sponsors

--- 2023kyoto/ejs.config.js
+++ 2023kyoto/ejs.config.js
@@ -18,5 +18,191 @@ module.exports = {
           '法学部法律学科からエンジニアへ転身し、2011年にDeNAに入社。Mobageおよび協業プラットフォームの大規模システム開発、オートモーティブ事業本部の開発責任者を歴任。2018年より執行役員としてDeNAのエンジニアリングの統括を務め、2019年より常務執行役員 CTOとしてより経営レベルでの意思決定にかかわることと、技術・モノづくりの強化を担う。2020年より常務執行役員として技術領域を担当しつつ、メディカル領域の新規事業を担当。',
       },
     ],
+    sponsors: [
+      {
+        key: 'perl',
+        class: 'basis-full',
+        icon_class: 'max-w-[300px]',
+        label: 'Perl Sponsors',
+        companies: [
+          {
+            name: '株式会社ディー・エヌ・エー',
+            image: 'dena.png',
+            url: 'https://dena.com',
+          },
+          {
+            name: 'LINE株式会社',
+            image: 'line.png',
+            url: 'https://engineering.linecorp.com/ja/',
+          },
+        ],
+      },
+      {
+        key: 'gold',
+        label: 'Gold Sponsors',
+        class: 'basis-full',
+        icon_class: 'max-w-[200px]',
+        companies: [
+          {
+            name: '株式会社ガイアックス',
+            image: 'gaiax.png',
+            url: 'https://www.gaiax.co.jp/',
+          },
+          {
+            name: '株式会社ミクシィ',
+            image: 'mixi.png',
+            url: 'https://mixi.co.jp/recruit/',
+          },
+          {
+            name: '株式会社フリークアウト',
+            image: 'freakout.png',
+            url: 'https://www.fout.co.jp/freakout/',
+          },
+          {
+            name: '株式会社はてな',
+            image: 'hatena.png',
+            url: 'https://hatenacorp.jp/',
+          },
+          {
+            name: '日本ピザハット株式会社',
+            image: 'pizza-hut.png',
+            url: 'https://www.pizzahut.jp/',
+          },
+          {
+            name: 'Nota株式会社',
+            image: 'nota.png',
+            url: 'https://notainc.com/ja/home',
+          },
+        ],
+      },
+      {
+        key: 'silver',
+        label: 'Silver Sponsors',
+        class: 'basis-full',
+        icon_class: 'max-w-[150px]',
+        companies: [
+          {
+            name: 'Have Fun Tech LLC',
+            image: 'havefuntech.png',
+            url: 'https://have-fun.tech/',
+          },
+          {
+            name: '株式会社COLSIS（コルシス）',
+            image: 'colsis.png',
+            url: 'https://colsis.jp/',
+          },
+          {
+            name: 'こだまリサーチ株式会社',
+            image: 'kodama.png',
+            url: 'https://www.kodamari.com/',
+          },
+          {
+            name: 'シックス・アパート株式会社',
+            image: 'six-apart.png',
+            url: 'https://www.sixapart.jp/',
+          },
+          {
+            name: '株式会社モバイルファクトリー',
+            image: 'mobile-factory.png',
+            url: 'https://www.mobilefactory.jp/',
+          },
+          {
+            name: '株式会社ネコトーストラボ',
+            image: 'neko-toast-lab.png',
+            url: 'https://nekotoast-lab.com/',
+          },
+          {
+            name: '面白法人カヤック',
+            image: 'kayac.png',
+            url: 'https://www.kayac.com/company/infographics ',
+          },
+          {
+            name: '株式会社トラーナ',
+            image: 'torana.png',
+            url: 'http://toysub.net/',
+          },
+          {
+            name: 'Wano株式会社',
+            image: 'wano.png',
+            url: 'https://group.wano.co.jp/',
+          },
+          {
+            name: '株式会社メディカルノート',
+            image: 'medicalnote.png',
+            url: 'https://medicalnote.co.jp',
+          },
+        ],
+      },
+      {
+        key: 't-shirt',
+        label: 'イベントTシャツ',
+        class: 'md:basis-64 basis-full',
+        icon_class: 'max-w-[150px]',
+        companies: [
+          {
+            name: '株式会社モバイルファクトリー',
+            image: 'mobile-factory.png',
+            url: 'https://www.mobilefactory.jp/',
+          },
+        ],
+      },
+      {
+        key: 'best-talk',
+        label: 'ベストトーク',
+        class: 'md:basis-64 basis-full',
+        icon_class: 'max-w-[150px]',
+        companies: [
+          {
+            name: 'Nota株式会社',
+            image: 'nota.png',
+            url: 'https://notainc.com/ja/home',
+          },
+        ],
+      },
+      {
+        key: 'best-lt',
+        label: 'ベストLT',
+        class: 'md:basis-64 basis-full',
+        icon_class: 'max-w-[150px]',
+        companies: [
+          {
+            name: 'Have Fun Tech LLC',
+            image: 'havefuntech.png',
+            url: 'https://have-fun.tech/',
+          },
+        ],
+      },
+      {
+        key: 'student-support',
+        label: '学生支援',
+        class: 'md:basis-64 basis-full',
+        icon_class: 'max-w-[150px]',
+        companies: [
+          {
+            name: '面白法人カヤック',
+            image: 'kayac.png',
+            url: 'https://www.kayac.com/company/infographics',
+          },
+          {
+            name: 'MyAnimeList',
+            image: 'myanimelist.png',
+            url: 'https://myanimelist.net/',
+          },
+        ],
+      },
+      {
+        key: 'original-emoji',
+        label: 'オリジナル絵文字',
+        class: 'md:basis-64 basis-full',
+        icon_class: 'max-w-[150px]',
+        companies: [
+          {
+            name: '株式会社ネコトーストラボ',
+            image: 'neko-toast-lab.png',
+            url: 'https://nekotoast-lab.com/',
+          },
+        ],
+      },
+    ],
   },
 };
