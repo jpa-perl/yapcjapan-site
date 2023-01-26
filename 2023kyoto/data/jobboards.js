8efa501a12c560eecf6a8843b1df50f374db1d65
nagayama <nagayama@iroirodesign.com>
2023-01-26 18:36:07 +0900

    Add the JobBoard section

---
+++ 2023kyoto/data/jobboards.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    src: 'https://picsum.photos/id/10/1200/600',
+    alt: 'Example 1',
+    url: 'https://example.com',
+  },
+  {
+    src: 'https://picsum.photos/id/20/1200/600',
+    alt: 'Example 2',
+    url: 'https://example.com',
+  },
+  {
+    src: 'https://picsum.photos/id/30/1200/600',
+    alt: 'Example 3',
+    url: 'https://example.com',
+  },
+  {
+    src: 'https://picsum.photos/id/40/1200/600',
+    alt: 'Example 4',
+    url: 'https://example.com',
+  },
+  {
+    src: 'https://picsum.photos/id/50/1200/600',
+    alt: 'Example 5',
+    url: 'https://example.com',
+  },
+];
