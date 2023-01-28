5ffd20e1d656dc463e0892fe92d376cfa521e587
papix <papix@hatena.ne.jp>
2023-01-28 11:45:50 +0900

    ジョブボード追加

--- 2023kyoto/data/jobboards.js
+++ 2023kyoto/data/jobboards.js
@@ -1,27 +1,12 @@
 module.exports = [
   {
-    src: 'https://picsum.photos/id/10/1200/600',
-    alt: 'Example 1',
-    url: 'https://example.com',
+    src: 'dena.png',
+    alt: '株式会社ディー・エヌ・エー',
+    url: 'https://go.dena.my/7C9uEQJ',
   },
   {
-    src: 'https://picsum.photos/id/20/1200/600',
-    alt: 'Example 2',
-    url: 'https://example.com',
-  },
-  {
-    src: 'https://picsum.photos/id/30/1200/600',
-    alt: 'Example 3',
-    url: 'https://example.com',
-  },
-  {
-    src: 'https://picsum.photos/id/40/1200/600',
-    alt: 'Example 4',
-    url: 'https://example.com',
-  },
-  {
-    src: 'https://picsum.photos/id/50/1200/600',
-    alt: 'Example 5',
-    url: 'https://example.com',
+    src: 'mobile-factory.png',
+    alt: '株式会社モバイルファクトリー',
+    url: 'https://recruit.mobilefactory.jp/',
   },
 ];
