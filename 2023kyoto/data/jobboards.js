34608d82ae2d72a9e54f4f4e7955f5d845f0fc27
papix <papix@hatena.ne.jp>
2023-02-01 08:44:59 +0900

    ジョブボード追加

--- 2023kyoto/data/jobboards.js
+++ 2023kyoto/data/jobboards.js
@@ -5,8 +5,23 @@ module.exports = [
     url: 'https://go.dena.my/7C9uEQJ',
   },
   {
+    src: 'hatena.jpg',
+    alt: '株式会社はてな',
+    url: 'https://hatena.co.jp/recruit/engineer',
+  },
+  {
     src: 'mobile-factory.png',
     alt: '株式会社モバイルファクトリー',
     url: 'https://recruit.mobilefactory.jp/',
+  },
+  {
+    src: 'prtimes.png',
+    alt: '株式会社PR TIMES',
+    url: 'https://developers.prtimes.jp/',
+  },
+  {
+    src: 'kayac.png',
+    alt: '株式会社カヤック',
+    url: 'https://www.kayac.com/recruit/career/works/1',
   },
 ];
