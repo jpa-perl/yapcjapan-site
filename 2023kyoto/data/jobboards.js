546a60a45ab1352cf9a79bac684b6eb6ae68ad7c
karupanerura <karupa@cpan.org>
2023-02-12 14:19:37 +0900

    add helpfeel jobboard

--- 2023kyoto/data/jobboards.js
+++ 2023kyoto/data/jobboards.js
@@ -29,4 +29,9 @@ module.exports = [
     alt: '株式会社カヤック',
     url: 'https://www.kayac.com/recruit/career/works/1',
   },
+  {
+    src: 'helpfeel.png',
+    alt: '株式会社Helpfeel',
+    url: 'https://scrapbox.io/helpfeel-dev-careers/%E9%96%8B%E7%99%BA%E9%83%A8_%E6%8E%A1%E7%94%A8%E6%83%85%E5%A0%B1',
+  },
 ];
