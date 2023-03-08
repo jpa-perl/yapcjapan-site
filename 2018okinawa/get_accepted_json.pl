0288d4e047120a3ba863034f53ca9964da116120
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2023-03-09 00:05:00 +0900

    CSV 列名から JSON キー名の変換ルールを採用して %keymap ハッシュを廃止

--- 2018okinawa/get_accepted_json.pl
+++ 2018okinawa/get_accepted_json.pl
@@ -3,21 +3,6 @@ use v5.36;
 use Text::CSV_XS qw(csv);
 use JSON::PP qw(encode_json);
 
-my %keymap = (
-    id          => 'gsx$id',
-    title       => 'gsx$title',
-    description => 'gsx$description',
-    talkTime    => 'gsx$talktime',
-    startAt     => 'gsx$startat',
-    trackId     => 'gsx$trackid',
-    name        => 'gsx$author',
-    timestamp   => 'gsx$timestamp', # not used
-    author      => 'gsx$author',
-    githubId    => 'gsx$githubid',
-    twitterId   => 'gsx$twitterid',
-    blogUrl     => 'gsx$blogurl',
-);
-
 my $speaker_csv_file = shift || "./speakers.csv";
 if ( !-f $speaker_csv_file ) {
     die "do not find $speaker_csv_file\n";
@@ -30,7 +15,7 @@ my $entries = [];
 for my $row (@$csv_data) {
     my $entry = {};
     for my $key (keys %$row) {
-        $entry->{ $keymap{$key} }->{'$t'} = $row->{$key};
+        $entry->{ 'gsx$' . lc($key) }->{'$t'} = $row->{$key};
     }
     push @$entries, $entry;
 }
