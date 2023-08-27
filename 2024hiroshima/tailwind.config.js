80af3fdd31d5a2162869c036c0b7d36f10ffd77f
nagayama <nagayama@iroirodesign.com>
2023-08-27 15:51:13 +0900

    apply basic styles

--- 2024hiroshima/tailwind.config.js
+++ 2024hiroshima/tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    'ejs.config.js',
-    './src/**/*.{ejs,js,ts,jsx,tsx}',
-    './dist/**/*.html',
+    "ejs.config.js",
+    "./src/**/*.{ejs,js,ts,jsx,tsx}",
+    "./dist/**/*.html",
   ],
   theme: {
     extend: {
       colors: {
-        bg: '#FFF',
-        jpa: '#10355E',
-        'bg-dark': '#EFEFEF',
-        'header-bg': 'rgba(255,255,255,0.3)',
+        black: "#222",
+        bg: "#FFF",
+        jpa: "#10355E",
+        red: "#C32121",
+        "bg-dark": "#EFEFEF",
+        "header-bg": "rgba(255,255,255,0.3)",
       },
     },
   },
