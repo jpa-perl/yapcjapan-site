4436d4bff53301eb3b37b3d5b6fe4e45420bdeb9
tkzwtks <tkzwtks@gmail.com>
2023-09-09 12:12:56 +0900

    スタッフ追加

--- 2024hiroshima/data/staff.js
+++ 2024hiroshima/data/staff.js
@@ -1,13 +1,63 @@
 const coreStaff = [
   {
+    name: 'kobaken',
+    avatar_url: './images/staff/twitter/kfly8.jpg',
+    profile_url: 'https://twitter.com/kfly8'
+  },
+  {
+    name: 'chanyou',
+    avatar_url: './images/staff/uploaded/chanyou.png',
+    profile_url: '',
+  },
+  {
+    name: 'nishimotz',
+    avatar_url: './images/staff/uploaded/nishimotoz.png',
+    profile_url: '',
+  },
+  {
+    name: 'mu2in',
+    avatar_url: './images/staff/uploaded/mu2in.png',
+    profile_url: '',
+  },
+  {
+    name: 'tamaclaw',
+    avatar_url: './images/staff/uploaded/tamaclaw.png',
+    profile_url: '',
+  },
+  {
+    name: 'kobakazu0429',
+    avatar_url: './images/staff/uploaded/kobakazu0429.png',
+    profile_url: '',
+  },
+  {
+    name: 'yicru',
+    avatar_url: './images/staff/uploaded/yicru.png',
+    profile_url: '',
+  },
+  {
+    name: 'karupanerura',
+    avatar_url: './images/staff/github/karupanerura.png',
+    profile_url: 'https://github.com/karupanerura'
+  },
+  {
     name: 'papix',
     avatar_url: './images/staff/twitter/__papix__.jpg',
     profile_url: 'https://twitter.com/__papix__'
   },
   {
-    name: 'kobaken',
-    avatar_url: './images/staff/twitter/kfly8.jpg',
-    profile_url: 'https://twitter.com/kfly8'
+    name: 'xtetsuji',
+    avatar_url: './images/staff/twitter/xtetsuji.png',
+    profile_url: 'https://twitter.com/xtetsuji'
+  },
+  {
+    name: 'liiu',
+    avatar_url: './images/staff/twitter/_ybrliiu.jpg',
+    profile_url: 'https://twitter.com/_ybrliiu'
+  },
+  {
+    name: 'tkzwtks',
+    avatar_url: './images/staff/uploaded/tkzwtks.jpg',
+    profile_url: 'https://twitter.com/tkzwtks'
   },
   {
     name: 'Pasta-K',
@@ -20,29 +70,29 @@ const coreStaff = [
     profile_url: 'https://twitter.com/nyanco15'
   },
   {
-    name: 'tkzwtks',
-    avatar_url: './images/staff/uploaded/tkzwtks.jpg',
-    profile_url: 'https://twitter.com/tkzwtks'
+    name: 'godan',
+    avatar_url: './images/staff/twitter/godan09.jpg',
+    profile_url: 'https://twitter.com/godan09'
+  },
+  {
+    name: 'toya',
+    avatar_url: './images/staff/uploaded/toya.png',
+    profile_url: ''
+  },
+  {
+    name: 'maeken2010',
+    avatar_url: './images/staff/uploaded/maeken2010.png',
+    profile_url: ''
+  },
+  {
+    name: 'tomcha',
+    avatar_url: './images/staff/uploaded/tomcha.jpg',
+    profile_url: 'https://twitter.com/tomcha_'
   },
   {
     name: 'nagayama',
     avatar_url: './images/staff/twitter/nagayama.jpg',
     profile_url: 'https://twitter.com/nagayama'
-  },
-  {
-    name: 'karupanerura',
-    avatar_url: './images/staff/github/karupanerura.png',
-    profile_url: 'https://github.com/karupanerura'
-  },
-  {
-    name: 'xtetsuji',
-    avatar_url: './images/staff/twitter/xtetsuji.png',
-    profile_url: 'https://twitter.com/xtetsuji'
-  },
-  {
-    name: 'godan',
-    avatar_url: './images/staff/twitter/godan09.jpg',
-    profile_url: 'https://twitter.com/godan09'
   },
   {
     name: 'mazco',
