049226ed3bc2a87f64eb0e1e2392e99e35e457f1
nagayama <nagayama@iroirodesign.com>
2023-09-11 00:17:38 +0900

    Use instance mode of p5.js

--- 2024hiroshima/src/partial/bg.js
+++ 2024hiroshima/src/partial/bg.js
@@ -1,134 +1,141 @@
-let canvas;
-let shapes = [];
-let centerX;
-let objectNum;
+import p5 from "p5";
 
-const COLOR = "#C32121";
+let sketch = function (p) {
+  let canvas;
+  let shapes = [];
+  let centerX;
+  let objectNum;
 
-function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent("p5-container");
-  pixelDensity(1);
-  init();
-}
+  const COLOR = "#C32121";
 
-function init() {
-  centerX = width / 2;
-  objectNum = floor(map(width, 0, 1920, 10, 60));
-}
+  const ShapeType = {
+    CIRCLE: "circle",
+    RECT: "rect",
+    TRIANGLE: "triangle",
+  };
 
-function draw() {
-  background(255);
-  push();
-  translate(centerX, 0);
-  for (let i = 0; i < objectNum; i++) {
-    if (shapes[i] == null) {
-      shapes[i] = new Shape();
-    }
-    const shape = shapes[i];
-    if (i > objectNum) {
-      shape.setShow(false);
-    } else {
-      shape.setShow(true);
-    }
-    shape.draw();
+  p.setup = function () {
+    canvas = p.createCanvas(p.windowWidth, p.windowHeight);
+    canvas.parent("p5-container");
+    p.pixelDensity(1);
+    init();
+  };
+
+  function init() {
+    centerX = p.width / 2;
+    objectNum = p.floor(p.map(p.width, 0, 1920, 10, 60));
   }
-  pop();
-}
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  init();
-}
+  p.draw = function () {
+    p.background(255);
+    p.push();
+    p.translate(centerX, 0);
+    for (let i = 0; i < objectNum; i++) {
+      if (shapes[i] == null) {
+        shapes[i] = new Shape(p);
+      }
+      const shape = shapes[i];
+      if (i > objectNum) {
+        shape.setShow(false);
+      } else {
+        shape.setShow(true);
+      }
+      shape.draw();
+    }
+    p.pop();
+  };
 
-const ShapeType = {
-  CIRCLE: "circle",
-  RECT: "rect",
-  TRIANGLE: "triangle",
+  p.windowResized = function () {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+    init();
+  };
+
+  class Shape {
+    constructor(p) {
+      this.p = p;
+      this.shapeType = p.random(Object.values(ShapeType));
+      this.x = p.random(-p.width / 2, p.width / 2);
+      this.y = p.random(p.height);
+      this.z = p.random(0, 100);
+      this.acl = p.map(this.z, 0, 100, 0.5, 2);
+      this.size = p.map(this.z, 0, 100, 10, 100);
+      this.rotation = p.random(p.TAU);
+      this.rotAcl = p.random(-0.01, 0.01);
+      this.color = p.color(COLOR);
+      this.strokeWeight = p.map(this.size, 10, 100, 2, 8);
+      this.targetOpacity = 255;
+      this.show = true;
+      this.opacity = 0;
+    }
+
+    reset() {
+      this.shapeType = p.random(Object.values(ShapeType));
+      this.x = p.random(-p.width / 2, p.width / 2);
+      this.rotation = p.random(p.TAU);
+      this.rotAcl = p.random(-0.01, 0.01);
+    }
+
+    setShow(show) {
+      this.show = show;
+    }
+
+    update() {
+      this.y -= this.acl;
+      this.x += p.sin(this.y * 0.001 + this.size * 2) / 2;
+      const targetOpacity = this.show ? p.map(this.y, p.height, 0, 0, 30) : 0;
+      this.opacity += (targetOpacity - this.opacity) * 0.9;
+      this.color.setAlpha(this.opacity);
+      if (this.y < 0 - this.size) {
+        this.y = p.height + this.size;
+        this.reset();
+      }
+      this.rotation += this.rotAcl;
+    }
+
+    draw() {
+      this.update();
+      p.push();
+      p.rectMode(p.CENTER);
+      p.fill(255, 100);
+      p.strokeJoin(p.ROUND);
+      p.strokeWeight(this.strokeWeight);
+      p.stroke(this.color);
+      p.translate(this.x, this.y);
+      p.rotate(this.rotation);
+      switch (this.shapeType) {
+        case ShapeType.CIRCLE:
+          this.drawCircle();
+          break;
+        case ShapeType.RECT:
+          this.drawRect();
+          break;
+        case ShapeType.TRIANGLE:
+          this.drawTriangle();
+          break;
+      }
+      p.pop();
+    }
+
+    drawCircle() {
+      p.circle(0, 0, this.size);
+    }
+
+    drawRect() {
+      p.rect(0, 0, this.size, this.size);
+    }
+
+    drawTriangle() {
+      p.beginShape();
+      for (let i = 3; i > 0; i--) {
+        const R = (p.TAU / 3) * i;
+        const x = p.cos(R) * this.size;
+        const y = p.sin(R) * this.size;
+
+        p.vertex(x, y);
+      }
+      p.endShape(p.CLOSE);
+    }
+  }
 };
 
-class Shape {
-  constructor() {
-    this.shapeType = random(Object.values(ShapeType));
-    this.x = random(-width / 2, width / 2);
-    this.y = random(height);
-    this.z = random(0, 100);
-    this.acl = map(this.z, 0, 100, 0.5, 2);
-    this.size = map(this.z, 0, 100, 10, 100);
-    this.rotation = random(TAU);
-    this.rotAcl = random(-0.01, 0.01);
-    this.color = color(COLOR);
-    this.strokeWeight = map(this.size, 10, 100, 2, 8);
-    this.targetOpacity = 255;
-    this.show = true;
-    this.opacity = 0;
-  }
-
-  reset() {
-    this.shapeType = random(Object.values(ShapeType));
-    this.x = random(-width / 2, width / 2);
-    this.rotation = random(TAU);
-    this.rotAcl = random(-0.01, 0.01);
-  }
-
-  setShow(show) {
-    this.show = show;
-  }
-
-  update() {
-    this.y -= this.acl;
-    this.x += sin(this.y * 0.001 + this.size * 2) / 2;
-    const targetOpacity = this.show ? map(this.y, height, 0, 0, 30) : 0;
-    this.opacity += (targetOpacity - this.opacity) * 0.9;
-    this.color.setAlpha(this.opacity);
-    if (this.y < 0 - this.size) {
-      this.y = height + this.size;
-      this.reset();
-    }
-    this.rotation += this.rotAcl;
-  }
-
-  draw() {
-    this.update();
-    push();
-    rectMode(CENTER);
-    fill(255, 100);
-    strokeJoin(ROUND);
-    strokeWeight(this.strokeWeight);
-    stroke(this.color);
-    translate(this.x, this.y);
-    rotate(this.rotation);
-    switch (this.shapeType) {
-      case ShapeType.CIRCLE:
-        this.drawCircle();
-        break;
-      case ShapeType.RECT:
-        this.drawRect();
-        break;
-      case ShapeType.TRIANGLE:
-        this.drawTriangle();
-        break;
-    }
-    pop();
-  }
-
-  drawCircle() {
-    circle(0, 0, this.size);
-  }
-
-  drawRect() {
-    rect(0, 0, this.size, this.size);
-  }
-
-  drawTriangle() {
-    beginShape();
-    for (let i = 3; i > 0; i--) {
-      const R = (TAU / 3) * i;
-      const x = cos(R) * this.size;
-      const y = sin(R) * this.size;
-
-      vertex(x, y);
-    }
-    endShape(CLOSE);
-  }
-}
+new p5(sketch);
