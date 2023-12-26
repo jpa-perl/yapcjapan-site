6d7f111edc09cc6abd15c85dd5fe73ce67b93e78
chanybou0311 <chanyou0311@gmail.com>
2023-12-26 21:52:06 +0900

    ジョブボード追加

--- 2024hiroshima/data/jobboards.js
+++ 2024hiroshima/data/jobboards.js
@@ -20,6 +20,11 @@ module.exports = [
     url: "https://hrmos.co/pages/rizapgroup/jobs?category=1799629274565783552",
   },
   {
+    src: "job-draft.png",
+    alt: "転職ドラフト",
+    url: "https://recruit.livesense.co.jp/lp/engineer/overview",
+  },
+  {
     src: "six-apart.png",
     alt: "シックス・アパート株式会社",
     url: "https://www.sixapart.jp/jobs/",
@@ -45,9 +50,19 @@ module.exports = [
     url: "https://hello-world.smarthr.co.jp/",
   },
   {
+    src: "sakura.png",
+    alt: "さくらインターネット株式会社",
+    url: "https://www.sakura.ad.jp/lp/22engineer/",
+  },
+  {
     src: "kayac.png",
     alt: "面白法人カヤック",
     url: "https://www.kayac.com/news/2022/07/interview_sakata",
+  },
+  {
+    src: "timee.png",
+    alt: "株式会社タイミー",
+    url: "https://timee.notion.site/timee/Timee-Product-Org-Entrance-Book-b7380eb4f6954e29b2664fe6f5e775f9",
   },
   {
     src: "neko-toast-lab.png",
@@ -60,13 +75,38 @@ module.exports = [
     url: "https://www.fout.co.jp/recruit/freakout/career_tech/",
   },
   {
+    src: "dena-sompo-mobility.png",
+    alt: "株式会社DeNA SOMPO Mobility",
+    url: "https://herp.careers/v1/dsrecruit/sp7wBIoCmiLk",
+  },
+  {
     src: "not-a-hotel.png",
     alt: "NOT A HOTEL株式会社",
     url: "https://notahotel.com/careers",
   },
   {
+    src: "wine-to-nabe.png",
+    alt: "ワインと鍋",
+    url: "https://twitter.com/winenabe",
+  },
+  {
+    src: "smartbank.png",
+    alt: "株式会社スマートバンク",
+    url: "https://smartbank.co.jp/recruit/engineer-summary",
+  },
+  {
+    src: "studist.png",
+    alt: "株式会社スタディスト",
+    url: "https://studist-engineering.gitbook.io/entrance-book/",
+  },
+  {
     src: "bit-river.png",
     alt: "ビットリバー株式会社",
     url: "https://bitriver.jp/recruit",
+  },
+  {
+    src: "bullet-group.png",
+    alt: "バレットグループ株式会社",
+    url: "https://herp.careers/v1/bltinc/requisition-groups/11cb05a4-0f9c-4d35-aed9-767faa5edea5",
   },
 ];
