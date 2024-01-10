c2326d096daceca0b7d274a10d577fb8918fabcf
kobaken <kentafly88@gmail.com>
2024-01-10 18:20:06 +0900

    個人スポンサーのテンプレ用意

--- 2024hiroshima/data/individual-sponsors.js
+++ 2024hiroshima/data/individual-sponsors.js
@@ -1,5 +1,9 @@
 const individualSponsors = [
+  {
+    name: 'kobaken',
+    avatar_url: './images/staff/twitter/kfly8.jpg',
+    profile_url: 'https://twitter.com/kfly8'
+  },
 ];
-
 
 module.exports = individualSponsors
