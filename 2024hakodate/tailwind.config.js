5c46af955a4e8ce149b74c578a2cab9ecbcadb69
nagayama <nagayama@iroirodesign.com>
2024-05-19 18:46:34 +0900

    add animation

--- 2024hakodate/tailwind.config.js
+++ 2024hakodate/tailwind.config.js
@@ -1,4 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = {
+  jpa: "#10355E",
+  key: {
+    DEFAULT: "#004B6D",
+    50: "#A0E1FF",
+    100: "#8CDBFF",
+    200: "#63CEFF",
+    300: "#3AC2FF",
+    400: "#11B5FF",
+    500: "#009FE7",
+    600: "#0083BF",
+    700: "#006796",
+    800: "#004B6D",
+    900: "#002435",
+    950: "#001119",
+  },
+};
+
 module.exports = {
   content: [
     "ejs.config.js",
@@ -8,27 +27,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        jpa: "#10355E",
-        key: {
-          DEFAULT: "#004B6D",
-          50: "#A0E1FF",
-          100: "#8CDBFF",
-          200: "#63CEFF",
-          300: "#3AC2FF",
-          400: "#11B5FF",
-          500: "#009FE7",
-          600: "#0083BF",
-          700: "#006796",
-          800: "#004B6D",
-          900: "#002435",
-          950: "#001119",
-        },
+        ...colors,
       },
     },
     backgroundColor: {
+      ...colors,
       default: "#000",
     },
     textColor: {
+      ...colors,
       default: "#fff",
     },
     fontFamily: {
