72a5cbbf580e789af4faa74740b0ce0ce4575d19
Masayoshi Wada <developer@andantesoftware.com>
2024-06-22 02:49:50 +0900

    sync sponsors info
    add registered information and hide that cannot be posted

--- 2024hakodate/data/sponsors.js
+++ 2024hakodate/data/sponsors.js
@@ -1,29 +1,34 @@
 module.exports = [
+  /*
   {
     key: "perl",
     class: "basis-full",
     icon_class: "max-w-[300px]",
     label: "Perl Sponsors",
     companies: [
-      /*
       {
         name: "トグルホールディングス株式会社",
         url: "https://toggle.co.jp/",
-        image: "toggle.png",
+        image: "toggle.png"
       },
-      */
       {
         name: "株式会社ディー・エヌ・エー",
-        url: "https://engineering.dena.com/",
-        image: "dena.png",
+        url: "http://dena.com/",
+        image: "dena.png"
       },
       {
         name: "株式会社Helpfeel",
-        url: "https://corp.helpfeel.com/",
-        image: "helpfeel.png",
+        url: "https://corp.helpfeel.com",
+        image: "helpfeel.png"
       },
+      {
+        name: "newmo株式会社",
+        url: "https://newmo.me/",
+        image: "newmo.png"
+      }
     ]
   },
+  */
   {
     key: "platinum",
     class: "basis-full",
@@ -34,53 +39,153 @@ module.exports = [
       {
         name: "ピクシブ株式会社",
         url: "https://www.pixiv.co.jp/",
-        image: "pixiv.png",
+        image: "pixiv.png"
       },
       */
       {
         name: "株式会社hacomono",
         url: "https://coal-son-1e1.notion.site/Entrance-Book-hacomono-4f5f5cec269e4e60ae8a918bed653efd",
-        image: "hacomono.png",
+        image: "hacomono.png"
       },
+      /*
       {
         name: "さくらインターネット株式会社",
         url: "https://www.sakura.ad.jp/",
-        image: "sakura.png",
+        image: "sakura.png"
       },
-      /*
       {
         name: "Cloudbase株式会社",
         url: "https://cloudbase.co.jp/company",
-        image: "cloudbase.png",
+        image: "cloudbase.png"
       },
-      */
       {
         name: "株式会社LayerX",
-        url: "https://jobs.layerx.co.jp/",
-        image: "layerx.png",
+        url: "https://jobs.layerx.co.jp",
+        image: "layerx.png"
       },
-      /*
       {
         name: "株式会社DELTA",
-        url: "https://teamdelta.jp/",
-        image: "delta.png",
+        url: "https://teamdelta.jp",
+        image: "teamdelta.png"
       },
       {
         name: "株式会社カケハシ",
         url: "https://www.kakehashi.life/",
-        image: "kakehashi.png",
+        image: "kakehashi.png"
       },
-      */
       {
         name: "株式会社スマートバンク",
         url: "https://smartbank.co.jp/recruit/engineer-summary",
-        image: "smartbank.png",
+        image: "smartbank.png"
       },
       {
         name: "ファインディ株式会社",
         url: "https://findy.co.jp/",
-        image: "findy.png",
-      },
+        image: "findy.png"
+      }
+      */
     ]
   },
+  /*
+  {
+    key: "gold",
+    class: "basis-full",
+    icon_class: "max-w-[200px]",
+    label: "Gold Sponsors",
+    companies: [
+      {
+        name: "株式会社はてな",
+        url: "https://hatena.co.jp",
+        image: "hatena.png"
+      },
+      {
+        name: "シックス・アパート株式会社",
+        url: "https://www.sixapart.jp/",
+        image: "sixapart.png"
+      },
+      {
+        name: "株式会社フリークアウト",
+        url: "https://www.fout.co.jp/freakout/",
+        image: "fout.png"
+      }
+    ]
+  },
+  */
+  /*
+  {
+    key: "silver",
+    class: "basis-full",
+    icon_class: "max-w-[200px]",
+    label: "Silver Sponsors",
+    companies: [
+      {
+        name: "面白法人カヤック",
+        url: "https://hubspot.kayac.com/engineer-interviews",
+        image: "kayac.png"
+      },
+      {
+        name: "株式会社モバイルファクトリー",
+        url: "https://www.mobilefactory.jp/",
+        image: "mobilefactory.png"
+      }
+    ]
+  },
+  */
+  {
+    key: "bronze",
+    class: "basis-full",
+    icon_class: "max-w-[200px]",
+    label: "Bronze Sponsors",
+    companies: [
+      {
+        name: "合同会社Y.pm",
+        url: "https://www.ypm.llc/",
+        image: "ypm.png"
+      },
+      {
+        name: "note株式会社",
+        url: "https://note.jp/",
+        image: "note.png"
+      },
+      /*
+      {
+        name: "株式会社カオナビ",
+        url: "https://corp.kaonavi.jp/",
+        image: "kaonavi.png"
+      },
+      */
+      {
+        name: "合同会社ザウエル",
+        url: "https://zauel.co.jp",
+        image: "zauel.png"
+      },
+      /*
+      {
+        name: "ワインと鍋",
+        url: "https://winenabe.jp/",
+        image: "winenabe.png"
+      },
+      {
+        name: "有限会社アートライ",
+        url: "http://www.artry.co.jp",
+        image: "artry.png"
+      }
+      */
+    ]
+  },
+  /*
+  {
+    key: "local",
+    class: "basis-full",
+    icon_class: "max-w-[200px]",
+    label: "ご当地スポンサー",
+    companies: [
+      {
+        name: "こだまリサーチ株式会社",
+        url: "https://www.kodamari.com",
+        image: "kodamari.png"
+      }
+    ]
+  },
+  */
 ];
