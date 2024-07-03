ca0f21e7630a6c2370d87cb946f768bb5f6adb99
papix <mail@papix.com>
2024-07-03 11:33:22 +0900

    ゲストの紹介文言を修正

--- 2024hakodate/ejs.config.js
+++ 2024hakodate/ejs.config.js
@@ -32,7 +32,7 @@ module.exports = {
         description: `
 大学在学中の2013年よりLINE株式会社にてブログサービスや動画配信サービス、ウェブ広告配信システムなどの高トラフィックなウェブサービスの設計・開発に従事。その後、2017年に異業種であるテレコムスタートアップの株式会社ソラコムに転職し、セルラーコアネットワークを中心とする複数のソフトウェアコンポーネントの設計・開発を担当。
 2024年6月より現職。家計簿プリカ「B/43」を中心としたソフトウェアプロダクトの開発を行なっている。
-2015年The Perl Foundation Grants Committee ProgramにてPerl::Lintが採択。builderscon Tokyo 2018にてベストスピーカー賞を受賞。
+2015年The Perl Foundation Grants Committee ProgramにてPerl::Lintが採択。builderscon Tokyo 2018にてベストスピーカー賞を受賞。北海道函館市出身。
           `,
       },
     ],
