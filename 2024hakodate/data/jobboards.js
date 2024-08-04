e0876e9bfa3aefa1aa483fd24aeadd079ac2986d
karupanerura <karupa@cpan.org>
2024-08-04 18:33:51 +0900

    add job boards

--- 2024hakodate/data/jobboards.js
+++ 2024hakodate/data/jobboards.js
@@ -1,1 +1,110 @@
-module.exports = [];
+module.exports = [
+  {
+    src: "toggle.png",
+    alt: "トグルホールディングス株式会社",
+    url: "https://toggle.co.jp/position/engineer/",
+  },
+  {
+    src: "dena.png",
+    alt: "株式会社ディー・エヌ・エー",
+    url: "https://go.dena.my/7C9uEQJ",
+  },
+  {
+    src: "helpfeel.png",
+    alt: "株式会社Helpfeel",
+    url: "https://recruit.helpfeel.com/",
+  },
+  /*
+  {
+    src: "newmo.png",
+    alt: "newmo株式会社",
+    url: "",
+  },
+  */
+  {
+    src: "pixiv.png",
+    alt: "ピクシブ株式会社",
+    url: "https://www.pixiv.co.jp/",
+  },
+  {
+    src: "hacomono.png",
+    alt: "株式会社hacomono",
+    url: "https://coal-son-1e1.notion.site/Entrance-Book-hacomono-4f5f5cec269e4e60ae8a918bed653efd",
+  },
+  {
+    src: "sakura.png",
+    alt: "さくらインターネット株式会社",
+    url: "https://www.sakura.ad.jp/lp/recruit-engineer/",
+  },
+  {
+    src: "cloudbase.png",
+    alt: "Cloudbase株式会社",
+    url: "https://www.notion.so/levetty/Cloudbase-Engineer-Entrance-Book-1f51dcaf9edd490e934780f526f833a3?pvs=4",
+  },
+  /*
+  {
+    src: "layerx.png",
+    alt: "株式会社LayerX",
+    url: "",
+  },
+  {
+    src: "delta.png",
+    alt: "株式会社DELTA",
+    url: "",
+  },
+  */
+  {
+    src: "kakehashi.jpeg",
+    alt: "株式会社カケハシ",
+    url: "https://hrmos.co/pages/kakehashi/jobs",
+  },
+  /*
+  {
+    src: "smartbank.png",
+    alt: "株式会社スマートバンク",
+    url: "",
+  },
+  */
+  {
+    src: "findy.jpeg",
+    alt: "ファインディ株式会社",
+    url: "https://herp.careers/v1/findy/requisition-groups/14c4a661-5e48-40c5-99d0-ea657b8b4c04",
+  },
+  {
+    src: "hatena.jpeg",
+    alt: "株式会社はてな",
+    url: "https://hatena.co.jp/recruit/engineer",
+  },
+  {
+    src: "sixapart.png",
+    alt: "シックス・アパート株式会社",
+    url: "https://www.sixapart.jp/jobs/",
+  },
+  {
+    src: "freakout.png",
+    alt: "株式会社フリークアウト",
+    url: "https://www.fout.co.jp/recruit/freakout/career_tech/",
+  },
+  {
+    src: "kayac.png",
+    alt: "株式会社カヤック",
+    url: "https://www.kayac.com/news/2022/07/interview_sakata",
+  },
+  {
+    src: "mobilefactory.png",
+    alt: "株式会社モバイルファクトリー",
+    url: "https://recruit.mobilefactory.jp/",
+  },
+  /*
+  {
+    src: "colsis.png",
+    alt: "株式会社コルシス",
+    url: "",
+  },
+  {
+    src: "nekotoast-lab.png",
+    alt: "株式会社ネコトーストラボ",
+    url: "",
+  },
+  */
+];
