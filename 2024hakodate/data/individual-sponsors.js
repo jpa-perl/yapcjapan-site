0491997bd74c7c93a4fd0eea40deef89a927aacf
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2024-09-30 11:45:03 +0000

    個人スポンサーのSNS画像とページ情報を追加

--- 2024hakodate/data/individual-sponsors.js
+++ 2024hakodate/data/individual-sponsors.js
@@ -1,1 +1,214 @@
-module.exports = [];
+const indivdualSponsors = [
+    {
+        name: 'makky12',
+        avatar_url: './images/individual-sponsors/makky12.jpg',
+        profile_url: 'https://x.com/makky12'
+    },
+    {
+        name: 'sironekotoro',
+        avatar_url: './images/individual-sponsors/sironekotoro.png',
+        profile_url: 'https://x.com/sironekotoro'
+    },
+    {
+        name: 'niratama',
+        avatar_url: './images/individual-sponsors/niratama.jpeg',
+        profile_url: 'https://x.com/niratama'
+    },
+    {
+        name: 'tomio2480',
+        avatar_url: './images/individual-sponsors/tomio2480.png',
+        profile_url: 'https://x.com/tomio2480'
+    },
+    {
+        name: 'onk',
+        avatar_url: './images/individual-sponsors/onk.png',
+        profile_url: 'https://github.com/onk'
+    },
+    {
+        name: 'sotarok',
+        avatar_url: './images/individual-sponsors/sotarok.png',
+        profile_url: 'https://x.com/sotarok'
+    },
+    {
+        name: 'nasa9084',
+        avatar_url: './images/individual-sponsors/nasa9084.jpg',
+        profile_url: 'https://x.com/nasa9084'
+    },
+    {
+        name: 'shinotra',
+        avatar_url: './images/individual-sponsors/shinotra.png',
+        profile_url: 'https://x.com/shinotra'
+    },
+    {
+        name: 'toku_bass',
+        avatar_url: './images/individual-sponsors/toku_bass.png',
+        profile_url: 'https://x.com/toku_bass'
+    },
+    {
+        name: 'hmatsu47',
+        avatar_url: './images/individual-sponsors/hmatsu47.jpg',
+        profile_url: 'https://x.com/hmatsu47'
+    },
+    {
+        name: 'tsubasaxZZZ',
+        avatar_url: './images/individual-sponsors/tsubasaxZZZ.jpg',
+        profile_url: 'https://x.com/tsubasaxZZZ'
+    },
+    {
+        name: 's3i7h',
+        avatar_url: './images/individual-sponsors/s3i7h.jpg',
+        profile_url: 'https://x.com/s3i7h'
+    },
+    {
+        name: 'kazeburo',
+        avatar_url: './images/individual-sponsors/kazeburo.jpg',
+        profile_url: 'https://x.com/kazeburo'
+    },
+    {
+        name: 'songmu',
+        avatar_url: './images/individual-sponsors/songmu.jpeg',
+        profile_url: 'https://x.com/songmu'
+    },
+    {
+        name: 'utgwkk',
+        avatar_url: './images/individual-sponsors/utgwkk.jpg',
+        profile_url: 'https://x.com/utgwkk'
+    },
+    {
+        name: '941',
+        avatar_url: './images/individual-sponsors/941.jpg',
+        profile_url: 'https://x.com/941'
+    },
+    {
+        name: 'jitsu',
+        avatar_url: './images/individual-sponsors/jitsu.jpg',
+        profile_url: 'https://x.com/jitsu'
+    },
+    {
+        name: 'turugina',
+        avatar_url: './images/individual-sponsors/turugina.png',
+        profile_url: 'https://x.com/turugina'
+    },
+    {
+        name: 'fuba',
+        avatar_url: './images/individual-sponsors/fuba.jpg',
+        profile_url: 'https://x.com/fuba'
+    },
+    {
+        name: 'gennei',
+        avatar_url: './images/individual-sponsors/gennei.jpg',
+        profile_url: 'https://x.com/gennei'
+    },
+    {
+        name: 'teckl',
+        avatar_url: './images/individual-sponsors/teckl.jpg',
+        profile_url: 'https://x.com/teckl'
+    },
+    {
+        name: 'suno88',
+        avatar_url: './images/individual-sponsors/suno88.jpg',
+        profile_url: 'https://www.facebook.com/suno88'
+    },
+    {
+        name: 'noto',
+        avatar_url: './images/individual-sponsors/noto.jpg',
+        profile_url: 'https://x.com/noto'
+    },
+    {
+        name: 'takeyuweb',
+        avatar_url: './images/individual-sponsors/takeyuweb.jpg',
+        profile_url: 'https://x.com/takeyuweb'
+    },
+    {
+        name: 'masaya_dev',
+        avatar_url: './images/individual-sponsors/masaya_dev.jpg',
+        profile_url: 'https://x.com/masaya_dev'
+    },
+    {
+        name: 'ysaito8015',
+        avatar_url: './images/individual-sponsors/ysaito8015.jpg',
+        profile_url: 'https://x.com/ysaito8015'
+    },
+    {
+        name: 'hiratara',
+        avatar_url: './images/individual-sponsors/hiratara.jpg',
+        profile_url: 'https://x.com/hiratara'
+    },
+    {
+        name: 'maka2_donzoko',
+        avatar_url: './images/individual-sponsors/maka2_donzoko.png',
+        profile_url: 'https://x.com/maka2_donzoko'
+    },
+    {
+        name: 'asumikam',
+        avatar_url: './images/individual-sponsors/asumikam.jpg',
+        profile_url: 'https://x.com/asumikam'
+    },
+    {
+        name: 'fujiwara',
+        avatar_url: './images/individual-sponsors/fujiwara.jpg',
+        profile_url: 'https://x.com/fujiwara'
+    },
+    {
+        name: 'sugyan',
+        avatar_url: './images/individual-sponsors/sugyan.png',
+        profile_url: 'https://github.com/sugyan'
+    },
+    {
+        name: 'cohalz',
+        avatar_url: './images/individual-sponsors/cohalz.png',
+        profile_url: 'https://x.com/cohalz'
+    },
+    {
+        name: 'free_world21',
+        avatar_url: './images/individual-sponsors/free_world21.jpg',
+        profile_url: 'https://x.com/free_world21'
+    },
+    {
+        name: 'koba789',
+        avatar_url: './images/individual-sponsors/koba789.webp',
+        profile_url: 'https://misskey.io/@koba789'
+    },
+    {
+        name: 'mackee_w',
+        avatar_url: './images/individual-sponsors/mackee_w.jpg',
+        profile_url: 'https://x.com/mackee_w/'
+    },
+    {
+        name: 'magnolia_k_',
+        avatar_url: './images/individual-sponsors/magnolia_k_.jpg',
+        profile_url: 'https://x.com/magnolia_k_'
+    },
+    {
+        name: 'i47_rozary',
+        avatar_url: './images/individual-sponsors/i47_rozary.png',
+        profile_url: 'https://x.com/i47_rozary'
+    },
+    {
+        name: 'temama.bsky.social',
+        avatar_url: './images/individual-sponsors/temama.bsky.social.jpg',
+        profile_url: 'https://bsky.app/profile/temama.bsky.social'
+    },
+    {
+        name: 'bulkneets',
+        avatar_url: './images/individual-sponsors/bulkneets.jpg',
+        profile_url: 'https://x.com/bulkneets'
+    },
+    {
+        name: 'kanahiro_iguchi',
+        avatar_url: './images/individual-sponsors/kanahiro_iguchi.jpg',
+        profile_url: 'https://x.com/kanahiro_iguchi'
+    },
+    {
+        name: 'chan_san_jp',
+        avatar_url: './images/individual-sponsors/chan_san_jp.jpg',
+        profile_url: 'https://x.com/chan_san_jp'
+    },
+    {
+        name: 'yammerjp',
+        avatar_url: './images/individual-sponsors/yammerjp.jpg',
+        profile_url: 'https://x.com/yammerjp'
+    }
+];
+
+module.exports = indivdualSponsors
