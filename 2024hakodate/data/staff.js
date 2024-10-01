161216c3ea0c645d45773cb74db0ac8e80f236ee
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2024-10-01 14:34:33 +0000

    まずはコアスタッフでスタッフページを作成

--- 2024hakodate/data/staff.js
+++ 2024hakodate/data/staff.js
@@ -1,7 +1,63 @@
-const coreStaff = [];
+const coreStaff = [
+  {
+    name: 'karupanerura',
+    avatar_url: './images/staff/github/karupanerura.png',
+    profile_url: 'https://github.com/karupanerura'
+  },
+  {
+    name: 'ytnobody',
+    avatar_url: './images/staff/twitter/ytnobody.jpg',
+    profile_url: 'https://twitter.com/ytnobody'
+  },
+  {
+    name: 'papix',
+    avatar_url: './images/staff/twitter/__papix__.jpg',
+    profile_url: 'https://twitter.com/__papix__'
+  },
+  {
+    name: 'PerukiFUN',
+    avatar_url: './images/staff/twitter/PerukiFUN.jpg',
+    profile_url: 'https://twitter.com/PerukiFUN'
+  },
+  {
+    name: 'masawada',
+    avatar_url: './images/staff/uploaded/masawada.jpg',
+    profile_url: 'https://twitter.com/masawada'
+  },
+  {
+    name: 'liiu',
+    avatar_url: './images/staff/twitter/_ybrliiu.jpg',
+    profile_url: 'https://twitter.com/_ybrliiu'
+  },
+  {
+    name: 'toya',
+    avatar_url: './images/staff/uploaded/toya.png',
+    profile_url: 'https://twitter.com/toya'
+  },
+  {
+    name: 'nagayama',
+    avatar_url: './images/staff/twitter/nagayama.jpg',
+    profile_url: 'https://twitter.com/nagayama'
+  },
+  {
+    name: 'xtetsuji',
+    avatar_url: './images/staff/twitter/xtetsuji.png',
+    profile_url: 'https://twitter.com/xtetsuji'
+  },
+  {
+    name: 'godan',
+    avatar_url: './images/staff/twitter/godan09.jpg',
+    profile_url: 'https://twitter.com/godan09'
+  },
+  {
+    name: 'たかね',
+    avatar_url: './images/staff/twitter/takane28.jpeg',
+    profile_url: 'https://twitter.com/takanen28',
+  }
+];
 const volunteerStaff = [];
 
 module.exports = {
   coreStaff,
-  volunteerStaff,
+  // volunteerStaff,
 }
