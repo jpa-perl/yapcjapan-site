bfafead10e4b737612ee298ee9e5cc62aa42f495
nagayama <nagayama@iroirodesign.com>
2025-07-15 12:31:22 +0900

    add tailwind

--- 2025fukuoka/astro.config.mjs
+++ 2025fukuoka/astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  base: "/2025fukuoka/",
-  /* 公開時には↓のパスを設定してGitHub Pagesに公開する */
   //outDir: "../docs/2025fukuoka",
+  base: "/2025fukuoka/"
+  /* 公開時には↓のパスを設定してGitHub Pagesに公開する */,
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
