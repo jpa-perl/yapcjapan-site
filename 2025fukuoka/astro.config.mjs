d4dc416bf8773b1d037aaf553a012c25a5580194
nagayama <nagayama@iroirodesign.com>
2025-07-15 12:28:49 +0900

    Astro setup

---
+++ 2025fukuoka/astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  base: "/2025fukuoka/",
+  /* 公開時には↓のパスを設定してGitHub Pagesに公開する */
+  //outDir: "../docs/2025fukuoka",
+});
