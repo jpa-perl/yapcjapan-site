c962f0f14d41a0ab4ae71a816eb7d3fefa8b301a
nagayama <nagayama@iroirodesign.com>
2025-07-15 14:24:22 +0900

    上下に帯を追加

--- 2025fukuoka/astro.config.mjs
+++ 2025fukuoka/astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  //outDir: "../docs/2025fukuoka",
-  base: "/2025fukuoka/"
-  /* 公開時には↓のパスを設定してGitHub Pagesに公開する */,
+  base: "/2025fukuoka/",
+  /* 公開時には↓のパスを設定してGitHub Pagesに公開する 
+  outDir: "../docs/2025fukuoka",
+  */
 
   vite: {
     plugins: [tailwindcss()],
