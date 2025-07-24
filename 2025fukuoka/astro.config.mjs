737dbcf15cf1b45eea7dac39e03944c76bd3eacc
nagayama <nagayama@iroirodesign.com>
2025-07-24 18:52:36 +0900

    Heroセクションを分離

--- 2025fukuoka/astro.config.mjs
+++ 2025fukuoka/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   base: "/2025fukuoka/",
+  outDir: "dist/2025fukuoka",
   /* 公開時には↓のパスを設定してGitHub Pagesに公開する 
   outDir: "../docs/2025fukuoka",
   */
